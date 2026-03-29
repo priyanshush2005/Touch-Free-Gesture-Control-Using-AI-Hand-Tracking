f90070d3beb0f4019398e4a6351ef05122584bf8
Priyanshu Sharma <sharma.priyanshush2005@gmail.com>
2026-03-29 14:29:53 +0530

    added code of building UI and window setup

--- src/main.py
+++ src/main.py
@@ -24,3 +24,35 @@ WIN_W  = 1100
 WIN_H  = 660
 CAM_W  = 860
 CAM_H  = 580
+
+class GestureWaveApp(ctk.CTk):
+
+    def __init__(self):
+        super().__init__()
+
+        #Window Setup
+        self.title("GestureWave AI")
+        self.geometry(f"{WIN_W}x{WIN_H}")
+        self.resizable(False, False)
+        self.configure(fg_color="#0f0f1a")
+
+        #State Variable
+        self.camera_running   = False
+        self.cap              = None
+        self.current_gesture  = "NONE"
+        self.current_conf     = 0.0
+        self.current_hands    = 0
+        self.fps              = 0
+        self._fps_timer       = time.time()
+        self._fps_counter     = 0
+        self.last_action      = "—"
+
+        #Building UI
+        self._build_sidebar()
+        self._build_main_area()
+
+        #Start camera automatically
+        self.after(500, self.start_camera)
+
+        #Handle window close
+        self.protocol("WM_DELETE_WINDOW", self.on_close)
