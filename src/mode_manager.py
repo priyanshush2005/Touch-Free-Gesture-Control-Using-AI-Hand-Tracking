e2e24a631ed086720b4fae05830a90e944dde437
Vanshika <vnshika9520@gmail.com>
2026-04-16 13:51:34 +0530

    removed comments for better readabilty

--- src/mode_manager.py
+++ src/mode_manager.py
@@ -8,9 +8,9 @@ PRESENTATION_MODE = "presentation"
 MEDIA_MODE        = "media"
 
 #Internal state
-_current_mode         = MEDIA_MODE   # app starts in media mode
-_last_switch_time     = 0            # timestamp of last mode switch
-MODE_SWITCH_COOLDOWN  = 2.0          # seconds before mode can switch again
+_current_mode         = MEDIA_MODE   
+_last_switch_time     = 0            
+MODE_SWITCH_COOLDOWN  = 2.0          
 
 
 def get_mode():
